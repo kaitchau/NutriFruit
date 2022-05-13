3d7542d2fe08a46336ccd6e029c013b45e51f4eb
Kaitlyn Chau <kachau03@gmail.com>
2022-05-13 16:25:21 -0400

    added a search bar and text boxes

--- src/SearchAgainButton.js
+++ src/SearchAgainButton.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const SearchAgainButton = () => {
   return (
-    <div>
-      <button class="bg-sky-600 hover:bg-sky-700 ...">Search More</button>
+    <div class="absolute h-32 w-fit ...">
+      <div class="absolute inset-x-[750px] bottom-0 h-50 ...">
+        <button class="bg-sky-600 hover:bg-sky-700 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300...">
+          Search More
+        </button>
+      </div>
     </div>
   );
 };
