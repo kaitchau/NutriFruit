685967bfe667eebc3bbdf8ca827bb2e4427b4dbf
Kaitlyn Chau <kachau03@gmail.com>
2022-05-13 14:34:28 -0400

    added image place holdder

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <FruitImage></FruitImage>
       <SearchBar></SearchBar>
       <NutritionText></NutritionText>
-      {/* {<FunFact></FunFact>} */}
+      <FunFact></FunFact>
     </div>
   );
 }
--- src/FruitImage.js
+++ src/FruitImage.js
@@ -3,7 +3,9 @@ import React from "react";
 const FruitImage = () => {
   return (
     <div className="block">
-      <p className="text-3xl font-bold underline"> DD </p>
+      <p className="text-3xl font-bold underline">DD </p>
+      {/*<img class="object-center hover:object-top src="..." ">
+  </img> */}
     </div>
   );
 };
