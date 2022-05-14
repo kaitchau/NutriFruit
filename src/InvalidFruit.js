5f5c9178fb029afbfd8c20c19248f8086e76fbf1
Kaitlyn Chau <kachau03@gmail.com>
2022-05-13 20:36:33 -0400

    updated InvalidFruit page and added error message

--- src/InvalidFruit.js
+++ src/InvalidFruit.js
@@ -3,9 +3,9 @@ import React from "react";
 const NutritionText = () => {
   return (
     <div className="relative h-64 w-100 ...">
-      <div className="absolute inset-y-0 left-[250px] w-[500px] bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+      <div className="absolute inset-y-0 left-[250px] w-2/3 bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
         <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
-          Nutrition Facts
+          Error: Please search for a valid fruit name.
         </h3>
       </div>
     </div>
